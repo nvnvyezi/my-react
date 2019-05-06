9ee4317c0d10ee0aca2c21cf146e0e60dd921614
nvnvyezi <1187128658@qq.com>
2019-05-06 22:20:13 +0800

    裁剪逻辑ok

--- packages/cropper/tailor.tsx
+++ packages/cropper/tailor.tsx
@@ -44,6 +44,17 @@ export default function Tailor(props: ITailor) {
   function upCropper() {
     document.onmousemove = null
   }
+  // 裁剪框初始位置
+  function handleDown(
+    fn: (e: MouseEvent) => void,
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) {
+    e.stopPropagation()
+    cropperRef.current = { startX: e.clientX, startY: e.clientY }
+    document.onmousemove = fn
+    document.onmouseup = upCropper
+  }
+
   // 裁剪框移动
   function moveCropper(e: MouseEvent) {
     const init = cropperRef.current
@@ -114,13 +125,178 @@ export default function Tailor(props: ITailor) {
       left: moveX,
     })
   }
-  // 裁剪框初始位置
-  function startCropper(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
-    const startX = e.clientX
-    const startY = e.clientY
-    cropperRef.current = { startX, startY }
-    document.onmousemove = moveCropper
-    document.onmouseup = upCropper
+
+  // 右边/下边拉伸裁剪框越界判断
+  function _beyondJudgement(num: number) {
+    if (
+      cropper.size + num + cropper.top > imgInfo.height - 2 &&
+      cropper.size + num + cropper.left > tailorWidth - 2
+    ) {
+      return
+    }
+    if (cropper.size + num + cropper.top > imgInfo.height - 2) {
+      return setCropper({
+        ...cropper,
+        size: imgInfo.height - cropper.top - 2,
+      })
+    }
+    if (cropper.size + num + cropper.left > tailorWidth - 2) {
+      return setCropper({
+        ...cropper,
+        size: tailorWidth - cropper.left - 2,
+      })
+    }
+    return setCropper({
+      ...cropper,
+      size: cropper.size + num,
+    })
+  }
+
+  //右下角拉伸
+  function moveSE(e: MouseEvent) {
+    const init = cropperRef.current
+    const moveX = e.clientX - init.startX
+    const moveY = e.clientY - init.startY
+    const moveMax = moveX > moveY ? moveX : moveY
+    _beyondJudgement(moveMax)
+  }
+
+  //向下拉伸
+  function moveS(e: MouseEvent) {
+    const init = cropperRef.current
+    const moveY = e.clientY - init.startY
+    _beyondJudgement(moveY)
+  }
+
+  // 左边拉伸裁剪框越界判断
+  function _beyondJudgementL(num: number) {
+    if (
+      num > cropper.left &&
+      num + cropper.top + cropper.size > imgInfo.height - 2
+    ) {
+      return
+    }
+    if (num > cropper.left) {
+      return setCropper({
+        ...cropper,
+        size: cropper.size + cropper.left,
+        left: 0,
+      })
+    }
+    if (num + cropper.top + cropper.size > imgInfo.height - 2) {
+      return setCropper({
+        ...cropper,
+        size: imgInfo.height - cropper.top - 2,
+        left: cropper.left - (imgInfo.height - 2 - cropper.top - cropper.size),
+      })
+    }
+    return setCropper({
+      ...cropper,
+      size: cropper.size + num,
+      left: cropper.left - num,
+    })
+  }
+
+  // 向左下拉伸
+  function moveSW(e: MouseEvent) {
+    const init = cropperRef.current
+    const moveY = e.clientY - init.startY
+    const moveX = init.startX - e.clientX
+    const moveMax = moveX > moveY ? moveX : moveY
+    _beyondJudgementL(moveMax)
+  }
+
+  // 向左拉伸
+  function moveW(e: MouseEvent) {
+    const init = cropperRef.current
+    const moveX = init.startX - e.clientX
+    _beyondJudgementL(moveX)
+  }
+
+  // 上边拉伸裁剪框越界判断
+  function _beyondJudgementT(num: number) {
+    if (num > cropper.left && num > cropper.top) {
+      return
+    }
+    if (num > cropper.left) {
+      return setCropper({
+        size: cropper.size + cropper.left,
+        top: cropper.top - cropper.left,
+        left: 0,
+      })
+    }
+    if (num > cropper.top) {
+      return setCropper({
+        size: cropper.top + cropper.size,
+        top: 0,
+        left: cropper.left - cropper.top,
+      })
+    }
+    return setCropper({
+      size: cropper.size + num,
+      top: cropper.top - num,
+      left: cropper.left - num,
+    })
+  }
+  // 向左上拉伸
+  function moveNW(e: MouseEvent) {
+    const init = cropperRef.current
+    const moveY = init.startY - e.clientY
+    const moveX = init.startX - e.clientX
+    const moveMax = moveX > moveY ? moveX : moveY
+    _beyondJudgementT(moveMax)
+  }
+
+  //向上拉伸
+  function moveN(e: MouseEvent) {
+    const init = cropperRef.current
+    const moveY = init.startY - e.clientY
+    _beyondJudgementT(moveY)
+  }
+
+  // 上边拉伸裁剪框越界判断
+  function _beyondJudgementR(num: number) {
+    if (
+      num > cropper.top &&
+      cropper.size + num + cropper.left > tailorWidth - 2
+    ) {
+      return
+    }
+    if (cropper.size + num + cropper.left > tailorWidth - 2) {
+      return setCropper({
+        ...cropper,
+        top: cropper.top - (tailorWidth - cropper.left - cropper.size),
+        size: tailorWidth - cropper.left - 2,
+      })
+    }
+    if (num > cropper.top) {
+      return setCropper({
+        ...cropper,
+        size: cropper.top + cropper.size,
+        top: 0,
+      })
+    }
+    return setCropper({
+      ...cropper,
+      size: cropper.size + num,
+      top: cropper.top - num,
+    })
+  }
+
+  //右上拉伸
+  function moveNE(e: MouseEvent) {
+    const init = cropperRef.current
+    const moveY = init.startY - e.clientY
+    const moveX = e.clientX - init.startX
+    const moveMax = moveX > moveY ? moveX : moveY
+    _beyondJudgementR(moveMax)
+  }
+
+  // 向右拉扩大裁剪框
+  function moveE(e: MouseEvent) {
+    const init = cropperRef.current
+    const moveX = e.clientX - init.startX
+    _beyondJudgementR(moveX)
   }
 
   return (
@@ -147,31 +323,58 @@ export default function Tailor(props: ITailor) {
             top: `${cropper.top}px`,
             left: `${cropper.left}px`,
           }}
-          onMouseDown={startCropper}
+          onMouseDown={e => handleDown(moveCropper, e)}
         >
-          <div className="ord-n resize-bar" />
-          <div className="ord-s resize-bar" />
           <div
-            className="ord-w resize-bar"
-            style={{ height: `${cropper.size}px` }}
+            className="ord-hn resize-bar"
+            onMouseDown={e => handleDown(moveN, e)}
           />
           <div
-            className="ord-e resize-bar"
-            style={{ height: `${cropper.size}px` }}
+            className="ord-hs resize-bar"
+            onMouseDown={e => handleDown(moveS, e)}
           />
-          <div className="ord-nw resize-handle" />
-          <div className="ord-n resize-handle" />
-          <div className="ord-ne resize-handle" />
-          <div className="ord-w resize-handle" />
-          <div className="ord-e resize-handle" />
-          <div className="ord-sw resize-handle" />
+          <div
+            className="ord-hw resize-bar"
+            style={{ height: `${cropper.size}px` }}
+            onMouseDown={e => handleDown(moveW, e)}
+          />
+          <div
+            className="ord-he resize-bar"
+            style={{ height: `${cropper.size}px` }}
+            onMouseDown={e => handleDown(moveE, e)}
+          />
+          <div
+            className="ord-nw resize-handle"
+            onMouseDown={e => handleDown(moveNW, e)}
+          />
+          <div
+            className="ord-n resize-handle"
+            onMouseDown={e => handleDown(moveN, e)}
+          />
+          <div
+            className="ord-ne resize-handle"
+            onMouseDown={e => handleDown(moveNE, e)}
+          />
+          <div
+            className="ord-w resize-handle"
+            onMouseDown={e => handleDown(moveW, e)}
+          />
+          <div
+            className="ord-e resize-handle"
+            onMouseDown={e => handleDown(moveE, e)}
+          />
+          <div
+            className="ord-sw resize-handle"
+            onMouseDown={e => handleDown(moveSW, e)}
+          />
           <div
             className="ord-s resize-handle"
-            onMouseDown={e => {
-              e.stopPropagation()
-            }}
+            onMouseDown={e => handleDown(moveS, e)}
           />
-          <div className="ord-se resize-handle" />
+          <div
+            className="ord-se resize-handle"
+            onMouseDown={e => handleDown(moveSE, e)}
+          />
         </div>
       </div>
     </section>
